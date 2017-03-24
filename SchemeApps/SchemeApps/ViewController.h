204ab53975379f1b7c9e76d4e258e7b534d90275
ireland2011 <312941925@qq.com>
2017-03-24 20:20:10 +0800

    -- 初始化项目

---
+++ SchemeApps/SchemeApps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchemeApps
+//
+//  Created by Genius on 24/3/2017.
+//  Copyright © 2017 Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SchemeApps/SchemeApps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SchemeApps
+//
+//  Created by Genius on 24/3/2017.
+//  Copyright © 2017 Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
